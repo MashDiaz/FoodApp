3c49f8034f9415e38edd47d98dfb18409d90e203
MashDiaz <maniyalive@gmail.com>
2024-08-08 18:20:32 +0530

    updated code

---
+++ app/src/main/java/com/example/foodapp/Dashboard.java
@@ -0,0 +1,115 @@
+package com.example.foodapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class Dashboard extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout dashboard, foods, drinks, beverages, cart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard2);
+
+        drawerLayout =findViewById(R.id.drawerlayout);
+        dashboard=findViewById(R.id.dashboard);
+        foods=findViewById(R.id.Food);
+        drinks=findViewById(R.id.drinks);
+        beverages=findViewById(R.id.beverages);
+        cart=findViewById(R.id.cart);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                openDrawer(drawerLayout);
+            }
+        });
+
+        dashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                recreate();
+            }
+        });
+
+        foods.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                redirectActivity(Dashboard.this, Foods.class);
+            }
+        });
+
+        drinks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                redirectActivity(Dashboard.this, Drinks.class);
+            }
+        });
+
+        beverages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                redirectActivity(Dashboard.this, Beverages.class);
+            }
+        });
+
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                redirectActivity(Dashboard.this, Cart.class);
+            }
+        });
+        beverages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(Dashboard.this, "Logout", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout)
+    {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout)
+    {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public static void redirectActivity(Activity activity, Class secondActivty)
+    {
+        Intent intent=new Intent(activity,secondActivty);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+
+}
