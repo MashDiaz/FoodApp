5ebdedfdf6dcbe8b0d679015a13cb08eea69612f
Codex_killer <imsasmithatharushan32@gmail.com>
2024-08-10 08:25:19 -0700

    added admin login,main dashboard and Item adding dashboard

--- app/src/main/java/com/example/foodapp/AdminDashboard.java
+++ app/src/main/java/com/example/foodapp/AdminDashboard.java
@@ -15,7 +15,7 @@ public class AdminDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.passAdminLoginpass), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
