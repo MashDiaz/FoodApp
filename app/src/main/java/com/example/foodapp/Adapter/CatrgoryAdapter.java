b332835e0a5e84c241b077e4cbf1d3dcb8ae6cf4
Matheesha Maduwantha <124491632+Mmaduwantha@users.noreply.github.com>
2024-08-11 13:01:23 +0530

    Glide ek ikuwa mata, dn hri

--- app/src/main/java/com/example/foodapp/Adapter/CatrgoryAdapter.java
+++ app/src/main/java/com/example/foodapp/Adapter/CatrgoryAdapter.java
@@ -10,7 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
+import com.bumptech.glide.Glide;
 
 import com.example.foodapp.Domain.CategoryDomain;
 import com.example.foodapp.R;
