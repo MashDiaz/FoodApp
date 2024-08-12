f153e47b0a5782d81945103a3229f7672db1337a
Codex_killer <imsasmithatharushan32@gmail.com>
2024-08-12 08:30:56 -0700

    manage users and map activities added, indents created

---
+++ app/src/main/java/com/example/foodapp/ManageUsersActivity.java
@@ -0,0 +1,20 @@
+package com.example.foodapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManageUsersActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_users);
+        // Code to retrieve and display the list of users
+    }
+
+    // Method to delete a user
+    private void deleteUser(String userId) {
+        // Code to delete the user from the database
+    }
+}
+
