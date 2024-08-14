0c13bdcf807698bf2e4fd3bcdb3284b87613f156
Matheesha Maduwantha <124491632+Mmaduwantha@users.noreply.github.com>
2024-08-14 20:33:32 +0530

    drink ekai food ekai wena wenama preview wenna ganna one ?

--- app/src/main/java/com/example/foodapp/Activity/Drinks.java
+++ app/src/main/java/com/example/foodapp/Activity/Drinks.java
@@ -1,29 +1,63 @@
 package com.example.foodapp.Activity;
 
-import android.app.Activity;
+import static com.example.foodapp.R.id.button;
+import static com.example.foodapp.R.layout.activity_drinks;
+import static com.example.foodapp.R.layout.activity_foods;
+
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.foodapp.Adapter.ItemAdapter;
 import com.example.foodapp.R;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Drinks extends AppCompatActivity {
 
-    DrawerLayout drawerLayout;
-    ImageView menu;
-    LinearLayout dashboard, foods, drinks, beverages, cart;
+    private RecyclerView recyclerView;
+    private ItemAdapter itemAdapter;
+    private List<Item> itemList;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drinks);
+        setContentView(activity_drinks);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        DBHelper dbHelper = new DBHelper(this);
+        itemList = dbHelper.getAllItems();
+        System.out.println(itemList);
+
+        itemAdapter = new ItemAdapter(this, itemList);
+        recyclerView.setAdapter(itemAdapter);
+
+        Button cartButton = findViewById(button);
+        cartButton.setOnClickListener(v -> addToCart());
     }
+
+    public void addToCart() {
+        ArrayList<String> itemNames = itemAdapter.getItemNames();
+        ArrayList<String> itemPrices = itemAdapter.getItemPrices();
+
+        // Intent to move to the cart activity
+        Intent intent = new Intent(Drinks.this, Cart.class);
+        intent.putStringArrayListExtra("cartNames", itemNames);
+        intent.putStringArrayListExtra("cartPrices", itemPrices);
+        startActivity(intent);
+
+    }
+
 
 }
